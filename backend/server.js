21e46174bd48546bc1dc5d40d10f4bc8832a9199
Bekzat O <bekzat@Bekzats-MBP.lan>
2022-05-09 22:02:19 -0700

    add controllers, middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv');
+const { errorHandler } = require('./middleware/errorMiddleware');
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes.js'));
+app.use(errorHandler);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
