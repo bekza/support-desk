098fd2b712a266818cfc9332b20a9a070f2a6b1b
Bekzat <beaconty@gmail.com>
2022-05-16 21:58:34 -0700

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors');
+const path = require('path');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -18,5 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__firname, '../frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) =>
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  );
+}
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
